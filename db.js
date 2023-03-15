54ce4a67a9cacaf33195d61ac291995c27968438
dziwwww <adzibilal02@gmail.com>
2023-03-15 13:38:10 +0700

    Port db custom

--- db.js
+++ db.js
@@ -4,14 +4,15 @@ const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const database = process.env.DB_NAME;
-
+const port = process.env.DB_PORT; // tambahkan variabel port
 
 // Buat koneksi
 const db = mysql.createConnection({
   host,
   user,
   password,
-  database
+  database,
+  port // tambahkan properti port
 });
 
 // Terhubung ke database
